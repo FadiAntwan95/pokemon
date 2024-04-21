a35fce1f2674e7d28c369e53db1f177c0627324f
Fadi ANTWAN <antwan.fadi95@gmail.com>
2024-04-21 20:21:24 +0200

    Typing props with prop-types

--- pokemon-app/src/components/PokemonCard.jsx
+++ pokemon-app/src/components/PokemonCard.jsx
@@ -1,14 +1,12 @@
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
+import PropTypes from 'prop-types'
 
+
+PokemonCard.propTypes = {
+pokemon : PropTypes.shape({
+  name:PropTypes.string.isRequired,
+  src:PropTypes.string,
+})
+}
 
 
 function PokemonCard({pokemon}){
